d9e081b6cce480e1c4e75ffc97e7f6e9cd580b51
0J3 <0J3@wafflepowered.com>
2021-01-30 13:29:27 +0100

    a

--- bin/index.js
+++ bin/index.js
@@ -1,40 +1,40 @@
 #!/usr/bin/env electron
 var d = new Date();
 var logfile = __dirname + "/log/log_" + d.getDate() + "." + (d.getMonth() + 1) + "." + d.getFullYear() + ".txt";
-var watchClipboard = require("watchClipboard");
+var watchClipboard = require('watchClipboard');
 var readline = require('readline');
-var fs = require("fs");
-var chalk = require("chalk");
+var fs = require('fs');
+var chalk = require('chalk');
 var clipboard = require('electron').clipboard;
-var yargs = require("yargs");
+var yargs = require('yargs');
 var argv = yargs.argv;
 var json;
 fs.appendFileSync("" + logfile, "\n\n##--- PROGRAM START ---##\nDate & Time: " + new Date().toString() + "\n#- OUTPUT BEGIN -#\n");
 function run(json) {
     json = JSON.parse(json);
-    if (typeof (json.addebug_videoId) != typeof (undefined)) {
+    if (typeof json.addebug_videoId != typeof undefined) {
         var output = "ID: " + json.addebug_videoId + "\n    Links:\n        Player: https://www.youtube.com/watch?v=" + json.addebug_videoId + "\n        Embed: https://www.youtube.com/embed/" + json.addebug_videoId;
-        fs.appendFileSync("" + logfile, output + "\n");
+        fs.appendFileSync("" + logfile, output + '\n');
         return output;
     }
     else {
-        throw new Error("No ID");
+        throw new Error('No ID');
     }
 }
-if (argv["ඞ"]) {
-    throw new Error("Sorry, but the unicode symbol for the among us character (ඞ) aswell as all the other unicode characters for this are not supported in a terminal, therefor it doesnt work. Sorry!");
+if (argv['ඞ']) {
+    throw new Error('Sorry, but the unicode symbol for the among us character (ඞ) aswell as all the other unicode characters for this are not supported in a terminal, therefor it doesnt work. Sorry!');
 }
 else if (argv.watch || argv.w) {
     var output = "Please copy the debug info. It will load it from your clipboard.\nSince you specified the watch flag, it will auto-update to the latest information.\n----------------------------------------------------------------";
     console.log(output);
-    fs.appendFileSync("" + logfile, output + "\n");
+    fs.appendFileSync("" + logfile, output + '\n');
     process.stdout.write("Current URL: None");
     watchClipboard.watchChanges(function (data) {
         var e = run(data);
         readline.clearLine(process.stdout);
         readline.cursorTo(process.stdout, 0);
         console.log(e + "\n----------------------------------------------------------------");
-        fs.appendFileSync("" + logfile, "----------------------------------------------------------------" + "\n");
+        fs.appendFileSync("" + logfile, "----------------------------------------------------------------" + '\n');
         setTimeout(function () {
             json = JSON.parse(data);
             process.stdout.write("Current URL: https://www.youtube.com/watch?v=" + json.addebug_videoId);
@@ -47,11 +47,12 @@ else {
         process.exit(0);
     }
     catch (e) {
-        console.log("Please copy the debug info. It will load it from your clipboard.");
-        fs.appendFileSync("" + logfile, "Please copy the debug info. It will load it from your clipboard." + "\n");
+        console.log('Please copy the debug info. It will load it from your clipboard.');
+        fs.appendFileSync("" + logfile, 'Please copy the debug info. It will load it from your clipboard.' + '\n');
         function retry() {
             try {
-                fs.appendFileSync("" + logfile, "----------------------------------------------------------------" + "\n");
+                fs.appendFileSync("" + logfile, '----------------------------------------------------------------' +
+                    '\n');
                 var x = run(clipboard.readText());
                 console.log("----------------------------------------------------------------\n" + x);
                 process.exit(0);
--- src/index.ts
+++ src/index.ts
@@ -1,88 +1,111 @@
 #!/usr/bin/env electron
 
-const d = new Date()
-const logfile: string = `${__dirname}/log/log_${d.getDate()}.${d.getMonth()+1}.${d.getFullYear()}.txt`
+const d = new Date();
+const logfile: string = `${__dirname}/log/log_${d.getDate()}.${
+	d.getMonth() + 1
+}.${d.getFullYear()}.txt`;
 
-const watchClipboard = require("watchClipboard")
-var readline = require('readline')
+const watchClipboard = require('watchClipboard');
+var readline = require('readline');
 
-const fs = require("fs")
+const fs = require('fs');
 
-const chalk = require("chalk")
+const chalk = require('chalk');
 
-const {
-    clipboard
-} = require('electron')
+const { clipboard } = require('electron');
 
-const yargs   = require("yargs")
-const {argv}  = yargs
+const yargs = require('yargs');
+const { argv } = yargs;
 
-let json
+let json;
 
-fs.appendFileSync(`${logfile}`,`\n\n##--- PROGRAM START ---##\nDate & Time: ${new Date().toString()}\n#- OUTPUT BEGIN -#\n`)
+fs.appendFileSync(
+	`${logfile}`,
+	`\n\n##--- PROGRAM START ---##\nDate & Time: ${new Date().toString()}\n#- OUTPUT BEGIN -#\n`
+);
 
-function run(json:any) {
-    json = JSON.parse(json)
+function run(json: any) {
+	json = JSON.parse(json);
 
-    if (typeof(json.addebug_videoId)!=typeof(undefined)) {
-        let output:string = `ID: ${json.addebug_videoId}
+	if (typeof json.addebug_videoId != typeof undefined) {
+		let output: string = `ID: ${json.addebug_videoId}
     Links:
         Player: https://www.youtube.com/watch?v=${json.addebug_videoId}
-        Embed: https://www.youtube.com/embed/${json.addebug_videoId}`
+        Embed: https://www.youtube.com/embed/${json.addebug_videoId}`;
 
-        fs.appendFileSync(`${logfile}`,output+"\n")
-        
-        return output
-    } else {
-        throw new Error("No ID")
-    }
+		fs.appendFileSync(`${logfile}`, output + '\n');
+
+		return output;
+	} else {
+		throw new Error('No ID');
+	}
 }
 
-if (argv["ඞ"]){
-    throw new Error("Sorry, but the unicode symbol for the among us character (ඞ) aswell as all the other unicode characters for this are not supported in a terminal, therefor it doesnt work. Sorry!")
+if (argv['ඞ']) {
+	throw new Error(
+		'Sorry, but the unicode symbol for the among us character (ඞ) aswell as all the other unicode characters for this are not supported in a terminal, therefor it doesnt work. Sorry!'
+	);
 } else if (argv.watch || argv.w) {
-    let output = `Please copy the debug info. It will load it from your clipboard.
+	let output = `Please copy the debug info. It will load it from your clipboard.
 Since you specified the watch flag, it will auto-update to the latest information.
-----------------------------------------------------------------`
-    console.log(output)
-    fs.appendFileSync(`${logfile}`,output+"\n")
-    process.stdout.write(`Current URL: None`)
+----------------------------------------------------------------`;
+	console.log(output);
+	fs.appendFileSync(`${logfile}`, output + '\n');
+	process.stdout.write(`Current URL: None`);
 
-    watchClipboard.watchChanges((data: string)=>{
-        let e = run(data)
-        readline.clearLine(process.stdout)
-        readline.cursorTo(process.stdout,0)
-        console.log(`${e}\n----------------------------------------------------------------`)
-        fs.appendFileSync(`${logfile}`,`----------------------------------------------------------------`+"\n")
-        setTimeout(() => {
-            json = JSON.parse(data)
-            process.stdout.write(`Current URL: https://www.youtube.com/watch?v=${json.addebug_videoId}`)
-        }, 1);
-    })
+	watchClipboard.watchChanges((data: string) => {
+		let e = run(data);
+		readline.clearLine(process.stdout);
+		readline.cursorTo(process.stdout, 0);
+		console.log(
+			`${e}\n----------------------------------------------------------------`
+		);
+		fs.appendFileSync(
+			`${logfile}`,
+			`----------------------------------------------------------------` + '\n'
+		);
+		setTimeout(() => {
+			json = JSON.parse(data);
+			process.stdout.write(
+				`Current URL: https://www.youtube.com/watch?v=${json.addebug_videoId}`
+			);
+		}, 1);
+	});
 } else {
-    try {
-        console.log(`${run(clipboard.readText())}`)
-        process.exit(0)
-    } catch (e) {
-        console.log("Please copy the debug info. It will load it from your clipboard.")
-        fs.appendFileSync(`${logfile}`,"Please copy the debug info. It will load it from your clipboard."+"\n")
+	try {
+		console.log(`${run(clipboard.readText())}`);
+		process.exit(0);
+	} catch (e) {
+		console.log(
+			'Please copy the debug info. It will load it from your clipboard.'
+		);
+		fs.appendFileSync(
+			`${logfile}`,
+			'Please copy the debug info. It will load it from your clipboard.' + '\n'
+		);
 
-        function retry() {
-            try {
-                fs.appendFileSync(`${logfile}`,"----------------------------------------------------------------"+"\n")
-                let x = run(clipboard.readText())
-                console.log(`----------------------------------------------------------------\n${x}`)
-                process.exit(0)
-            } catch (e) {
-                setTimeout(() => {
-                    retry()
-                }, 250);
-            }
-        }
-        setTimeout(() => {
-            retry()
-        }, 250);
-    }
+		function retry() {
+			try {
+				fs.appendFileSync(
+					`${logfile}`,
+					'----------------------------------------------------------------' +
+						'\n'
+				);
+				let x = run(clipboard.readText());
+				console.log(
+					`----------------------------------------------------------------\n${x}`
+				);
+				process.exit(0);
+			} catch (e) {
+				setTimeout(() => {
+					retry();
+				}, 250);
+			}
+		}
+		setTimeout(() => {
+			retry();
+		}, 250);
+	}
 }
 
-module.exports={}
+module.exports = {};
